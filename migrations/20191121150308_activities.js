05f959758fe41b2a83880d9208ccfc3f8ea15330
Student <student@eda-bc-06.localdomain>
2019-11-21 15:44:47 +1300

    database setup

--- migrations/20191121150308_activities.js
+++ migrations/20191121150308_activities.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('activities', (table) =>{
+      table.increments('activity_id'),
+      table.string('activity_name'),
+      table.string('snow'),
+      table.string('sleet'),
+      table.string('hail'),
+      table.string('thunder'),
+      table.string('heavy_rain'),
+      table.string('light_rain'),
+      table.string('showers'),
+      table.string('heavy_cloud'),
+      table.string('light_cloud')
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('activities')
 };
