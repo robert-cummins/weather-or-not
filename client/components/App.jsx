cf20d288ba7c13961e12ed6785da013769232c98
aliceals <alice.alsford@gmail.com>
2019-11-21 16:54:47 +1300

    add home component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,12 +4,14 @@ import { getWeather } from '../actions'
 
 import WeatherWeek from './WeatherWeek'
 import ActivityList from './ActivityList'
+import Home from './Home'
 
 
 const App = () => {
   return (
     <>
       <h1>Weather or Not</h1>
+      <Home />
       <WeatherWeek />
       <ActivityList />
     </>
