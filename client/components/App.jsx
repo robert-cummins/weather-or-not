223aabf8be90702d75a5a0b2706b4cd4f745c3e4
aliceals <alice.alsford@gmail.com>
2019-11-21 16:49:49 +1300

    add react components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getWeather } from '../actions'
 
+import WeatherWeek from './WeatherWeek'
+import ActivityList from './ActivityList'
+
 
 const App = () => {
   return (
-    <h1>Weather or Not</h1>
-
-
+    <>
+      <h1>Weather or Not</h1>
+      <WeatherWeek />
+      <ActivityList />
+    </>
   )
 }
 
