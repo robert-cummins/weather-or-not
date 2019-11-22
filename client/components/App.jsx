13e97face8a9762502382d5aef7166b1fa049602
coppinger <charles.coppinger@gmail.com>
2019-11-22 15:07:57 +1300

    Update style of weather an app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,33 +1,61 @@
-
-import React from 'react'
-import { connect } from 'react-redux'
-import { showPage } from '../actions'
-
-
+import React from "react";
+import { connect } from "react-redux";
+import { showPage } from "../actions";
 
 import WeatherWeek from "./WeatherWeek";
 
 import Home from "./Home";
 
 class App extends React.Component {
-
-  handleClick = (event) => {
-    event.preventDefault()
-    this.props.dispatch(showPage('home'))
-  }
+  handleClick = event => {
+    event.preventDefault();
+    this.props.dispatch(showPage("home"));
+  };
 
   render() {
     return (
       <>
-        <nav className="navbar bg-dark">
-          <button className="navbar-brand btn btn-light" onClick={this.handleClick} type="button">Home</button>
-        </nav>
-
-        {this.props.showPage == 'home' && <Home />}
-        {this.props.showPage == 'weather' && <><WeatherWeek /> </>}
-
-
-
+        <div class="pos-f-t">
+          <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+              <h5 class="text-white h4">Jeffery Epstein</h5>
+              <span class="text-muted">
+                didn't kill himself, he was murdered. Wake up.
+              </span>
+            </div>
+          </div>
+          <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#" onClick={this.handleClick}>
+              <img
+                src="/images/icons8-windy_weather.png"
+                width="30"
+                height="30"
+                class="d-inline-block align-top"
+                alt=""
+              />
+              Weather or Not
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </nav>
+        </div>
+        <div className="container-fluid mt-3">
+          {this.props.showPage == "home" && <Home />}
+          {this.props.showPage == "weather" && (
+            <>
+              <WeatherWeek />{" "}
+            </>
+          )}
+        </div>
       </>
     );
   }
@@ -36,8 +64,7 @@ class App extends React.Component {
 const mapStateToProps = state => {
   return {
     showPage: state.showPage
-  }
-}
-
+  };
+};
 
 export default connect(mapStateToProps)(App);
--- client/components/WeatherWeek.jsx
+++ client/components/WeatherWeek.jsx
@@ -3,80 +3,103 @@ import { connect } from "react-redux";
 
 import ActivityList from "./ActivityList";
 
-import { fetchWeather, setDayIndex } from '../actions'
+import { fetchWeather, setDayIndex } from "../actions";
 
 class WeatherWeek extends React.Component {
-
-
   componentDidMount() {
     this.props.dispatch(fetchWeather(this.props.selectedData.city));
   }
 
-
   render() {
-
-
     return (
       <>
         {this.props.weather ? (
           <section>
-            <h5>{this.props.selectedData.cityName}</h5>
-            <div className="container">
-              <ul
-                className="nav nav-pills nav-justified"
-                id="myTab"
-                role="tablist"
-              >
-                {this.props.weather.map((day, i) => {
-                  return (
-                    <>
-                      <li className="nav-item" key={i} id={day.date}>
-                        <a
-                          className="flex-sm-fill text-sm-center nav-link"
-                          id={`home-${i}-tab`}
-                          data-toggle="tab"
-                          href={`#home-${i}`}
-                          role="tab"
-                          aria-controls={`home-${i}`}
-                          aria-selected="true"
-                          onClick={() => this.props.dispatch(setDayIndex(i))}
-                        >
-                          <p>Date: {day.date}</p>
-                          <p>Weather: {day.weather}</p>
-                          {/* <img className="icon" src={day.weatherImg} /> */}
-                          <p>Temp: {Math.floor(day.temp)}</p>
-                          <p>Wind: {day.windDirection}</p>
-                          <p>Wind speed: {Math.floor(day.windSpeed)} km/h</p>
-                        </a>
-                      </li>
-                    </>
-                  );
-                })}
-              </ul>
-              <div className="tab-content" id="myTabContent">
-                {this.props.weather.map((day, i) => {
-                  return (
-                    <>
-                      <div
-                        key={i}
-                        id={day.date}
-                        className="tab-pane fade"
-                        id={`home-${i}`}
-                        role="tabpanel"
-                        aria-labelledby={`home-${i}-tab`}
+            <h3 className="mb-3">{this.props.selectedData.cityName}</h3>
+            <p className="text-muted">Can't beat it on a good day.</p>
+            <ul
+              className="nav nav-pills nav-justified"
+              id="myTab"
+              role="tablist"
+            >
+              {this.props.weather.map((day, i) => {
+                return (
+                  <>
+                    <li className="nav-item" key={i} id={day.date}>
+                      <a
+                        className="flex-sm-fill text-sm-center nav-link"
+                        id={`home-${i}-tab`}
+                        data-toggle="tab"
+                        href={`#home-${i}`}
+                        role="tab"
+                        aria-controls={`home-${i}`}
+                        aria-selected="true"
+                        onClick={() => this.props.dispatch(setDayIndex(i))}
                       >
-                        <ActivityList />
-                      </div>
-                    </>
-                  );
-                })}
-              </div>
+                        <img className="weather-icon" src={day.weatherImg} />
+                        <p className="text-muted">{day.weather}</p>
+                        <hr />
+                        <p className="text-muted">{day.date}</p>
+                        <p className="text-muted">
+                          <img
+                            src="/images/icons8-temperature.png"
+                            width="24"
+                            height="24"
+                            class="d-inline-block"
+                            alt=""
+                          />
+                          {Math.floor(day.temp)}°C
+                        </p>
+                        <p className="text-muted">
+                          <img
+                            src="/images/icons8-windsock.png"
+                            width="24"
+                            height="24"
+                            class="d-inline-block"
+                            alt=""
+                          />
+                          Wind: {day.windDirection}
+                        </p>
+                        <p className="text-muted">
+                          <img
+                            src="/images/icons8-air_element.png"
+                            width="24"
+                            height="24"
+                            class="d-inline-block"
+                            alt=""
+                          />
+                          Speed:
+                          <br />
+                          {Math.floor(day.windSpeed)} km/h
+                        </p>
+                      </a>
+                    </li>
+                  </>
+                );
+              })}
+            </ul>
+            <div className="tab-content" id="myTabContent">
+              {this.props.weather.map((day, i) => {
+                return (
+                  <>
+                    <div
+                      key={i}
+                      id={day.date}
+                      className="tab-pane fade"
+                      id={`home-${i}`}
+                      role="tabpanel"
+                      aria-labelledby={`home-${i}-tab`}
+                    >
+                      <ActivityList />
+                    </div>
+                  </>
+                );
+              })}
             </div>
-
           </section>
         ) : (
-            <h1>Data loading, please wait...</h1>
-          )}
+          <h1>Data loading, please wait...</h1>
+        )}
       </>
     );
   }
